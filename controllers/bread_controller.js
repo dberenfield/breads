2ee671e217874f5eefb8ac2367dbec471a385c04
Daniel Berenfield <danielberenfield@macbook-air.attlocal.net>
2023-09-06 19:31:12 -0400

    changes

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
 
 //INDEX
 breads.get('/', (req, res) => {
@@ -29,28 +30,41 @@ breads.post('/', (req, res) => {
   
 
 breads.get('/new', (req, res) => {
-    res.render('new')
+    Baker.find()
+        .then(foundBakers => {
+            res.render('new', {
+                bakers: foundBakers
+            })
+        })
+    
 })
 
 breads.get('/:id/edit', (req, res) => {
     console.log("Edit page")
-    Bread.findById(req.params.id)
-        .then(foundBread => {
-            res.render('edit', {
-                bread: foundBread
-            })
+    console.log("REQ.PARAMS.ID"+req.params.id)
+    Baker.find()
+        .then(foundBakers => {
+            Bread.findById(req.params.id)
+                .then(foundBread => {
+                    res.render('edit', {
+                        bread: foundBread,
+                        bakers: foundBakers
+                    })
+                })
         })
 })
 
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
+        .populate('baker')
         .then(foundBread => {
-            const bakedBy = foundBread.getBakedBy()
-            console.log(bakedBy)
+            // const bakedBy = foundBread.getBakedBy()
+            // console.log(bakedBy)
             res.render('show', {
                 bread: foundBread
             })
         }).catch(err => {
+            console.log(err)
             res.status(404).render('404')
         })
 })
@@ -86,6 +100,38 @@ breads.delete('/:id', (req, res) => {
     
     // Bread.splice(req.params.arrayIndex, 1)
     // res.status(303).redirect('/breads')
+})
+
+breads.get('/data/seed', (req,res) => {
+    Bread.insertMany([
+        {
+          name: 'Rye',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Rachel'
+        },
+        {
+          name: 'French',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+          baker: 'Joey'
+        },
+        {
+          name: 'Gluten Free',
+          hasGluten: false,
+          image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Ross'
+        },
+        {
+          name: 'Pumpernickel',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          baker: 'Phoebe'
+        }
+      ])
+        .then(createdBreads => {
+            res.redirect('/breads')
+        })
 })
 
 module.exports = breads
